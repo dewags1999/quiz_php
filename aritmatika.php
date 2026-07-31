8f13c0b987110348dacdd389ee6774e0106ce337
dewags1999 <94664861+dewags1999@users.noreply.github.com>
2026-08-01 06:19:58 +0800

    Menambahkan file latihan
    file berupa isi coding latihan php

---
+++ aritmatika.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BA243</title>
+</head>
+<body>
+    <?php
+    $a = 10;
+    $b = 3;
+
+    echo "Penjumlahan: " . ($a + $b) . "<br>";
+    echo "Pengurangan: " . ($a - $b) . "<br>";
+    echo "Perkalian: " . ($a * $b) . "<br>";
+    echo "Pembagian: " . ($a / $b) . "<br>";
+    echo "Modulus: " . ($a % $b) . "<br>";
+    echo "Pangkat: " . ($a ** $b) . "<br>";
+    ?>
+</body>
+</html>
---
+++ percabangan.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BA243</title>
+</head>
+<body>
+    <?php
+    $nilai = 85;
+    if ($nilai >= 90) {
+        echo "Grade: A";
+    } elseif ($nilai >= 80) {
+        echo "Grade: B";
+    } elseif ($nilai >= 70) {
+        echo "Grade: C";
+    } elseif ($nilai >= 60) {
+        echo "Grade: D";
+    } else {
+        echo "Grade: E";
+    }
+    ?>
+</body>
+</html>
